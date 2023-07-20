7a58cee58134b925f0005dfe15a519a07722cb91
Crowdbotics <team@crowdbotics.com>
2023-07-20 10:55:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "theworldnewsnetwork-42299",
+  url: "https://theworldnewsnetwork-42299.botics.co",
+  api: "https://theworldnewsnetwork-42299.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
